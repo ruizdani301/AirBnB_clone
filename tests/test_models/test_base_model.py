ad2cce793040817c2ac438230eca99a0b9d58149
tu nombre <ruizdani301@gmail.com>
2022-03-05 17:01:08 -0500

    correccion test base

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,53 +13,65 @@ class Test_Base_Model(unittest.TestCase):
        of the requirements of each method
     """
 
+    base = BaseModel()
+
+    def test_attributeExist(self):
+        """prove if attribute exist"""
+
+        self.assertTrue(hasattr(self.base, 'id'))
+        self.assertTrue(hasattr(self.base, 'updated_at'))
+        self.assertTrue(hasattr(self.base, 'created_at'))
+
+    def test_correctType(self):
+        """Verify if the type of the attribute is correct"""
+
+        self.assertIsInstance(self.base.id, str)
+        self.assertIsInstance(self.base.created_at, datetime)
+        self.assertIsInstance(self.base.updated_at, datetime)
+
     def test_created_class(self):
         """test when create a BaseModel
            validating each operation
         """
 
-        base = BaseModel()
-        self.assertEqual(BaseModel, type(base))
-        self.assertEqual(str, type(base.id))
-        self.assertEqual(datetime, type(base.created_at))
-        self.assertEqual(datetime, type(base.updated_at))
+        self.assertEqual(BaseModel, type(self.base))
+        self.assertEqual(str, type(self.base.id))
+        self.assertEqual(datetime, type(self.base.created_at))
+        self.assertEqual(datetime, type(self.base.updated_at))
 
     def test__init__kwargs(self):
         """test create BaseModel with dictionary
            validating each operation
         """
 
-        base = BaseModel()
-        dic = base.to_dict()
+        dic = self.base.to_dict()
         base2 = BaseModel(**dic)
-        self.assertEqual(base.id, base2.id)
-        self.assertNotEqual(base, base2)
-        base.number = 10
-        self.assertEqual(int, type(base.number))
-        base.nomber = "carlos"
-        self.assertEqual(str, type(base.nomber))
+        self.assertEqual(self.base.id, base2.id)
+        self.assertNotEqual(self.base, base2)
+        self.base.number = 10
+        self.assertEqual(int, type(self.base.number))
+        self.base.nomber = "carlos"
+        self.assertEqual(str, type(self.base.nomber))
 
     def test_to_dict(self):
         """test that check the dictionary
            validating each operation
         """
 
-        base = BaseModel()
-        dic = base.to_dict()
+        dic = self.base.to_dict()
         self.assertEqual(str, type(dic['updated_at']))
         self.assertEqual(str, type(dic['created_at']))
         self.assertEqual("BaseModel", dic['__class__'])
-        self.assertEqual(dict, type(base.to_dict()))
+        self.assertEqual(dict, type(self.base.to_dict()))
 
     def test_save(self):
         """test check method save()
            validating each operation
         """
 
-        base = BaseModel()
-        date = base.updated_at
-        base.save()
-        date2 = base.updated_at
+        date = self.base.updated_at
+        self.base.save()
+        date2 = self.base.updated_at
         self.assertNotEqual(date, date2)
 
 
